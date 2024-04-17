db21964dc511bf12f06d84ff38d89c3983c3b122
andrew verges <andrewverges0@gmail.com>
2024-04-17 15:57:01 +0000

    almost done code comments

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -20,77 +20,77 @@ var runLevels = function (window) {
     // BEGIN EDITING YOUR CODE HERE
   
 
-    function createSawBlade(x, y, damage){
-      var hitZoneSize = 25;
-    var damageFromObstacle = damage;
-    var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-    sawBladeHitZone.x = x;
-    sawBladeHitZone.y = y;
-    game.addGameItem(sawBladeHitZone);
-    var obstacleImage = draw.bitmap("img/sawblade.png");
-    sawBladeHitZone.addChild(obstacleImage);
-    obstacleImage.x = -25;
-    obstacleImage.y = -25;
+    function createSawBlade(x, y, damage){            //creates function for the obstacle
+      var hitZoneSize = 25;                      //sets a hitzone for the obstacle
+    var damageFromObstacle = damage;                     //makes the obstacle deal damage
+    var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);            //creates obstacle in the game
+    sawBladeHitZone.x = x;            //states the x value for the hitzone
+    sawBladeHitZone.y = y;             //states the y value for the hitzone
+    game.addGameItem(sawBladeHitZone);            //adds the obstacle to the game
+    var obstacleImage = draw.bitmap("img/sawblade.png");         //sets the image for the obstacle
+    sawBladeHitZone.addChild(obstacleImage);              //adds the image
+    obstacleImage.x = -25;              //sets x value for the obstacle
+    obstacleImage.y = -25;               //sets y value for the obstacle
     }
 
-    function createEnemy(x, y){
-      var enemy = game.createGameItem("enemy", 25);
-      var redSquare = draw.rect(50, 50, "red");
-      redSquare.x = -25;
-      redSquare.y = -25;
-      enemy.addChild(redSquare);
-      enemy.x = x;
-      enemy.y = y;
-      game.addGameItem(enemy);
-      enemy.velocityX = -4;
-      enemy.rotationalVelocity = 0;
+    function createEnemy(x, y){                       //creates function for the enemy
+      var enemy = game.createGameItem("enemy", 25);              //creates the enemy in the game
+      var redSquare = draw.rect(50, 50, "red");           //draws out a red square to be the enemy
+      redSquare.x = -25;                //sets the x value for the red square
+      redSquare.y = -25;                //sets the y value for the red square
+      enemy.addChild(redSquare);           //adds the red square to the game
+      enemy.x = x;            //sets x value for the enemy
+      enemy.y = y;             //sets y value for the enemy
+      game.addGameItem(enemy);         //adds the enemy to the game
+      enemy.velocityX = -4;        //speeds up the enemy to the left side of the screen
+      enemy.rotationalVelocity = 0;         //can let the square spin around
 
       
-      enemy.onPlayerCollision = function () {
-        game.changeIntegrity(-20);
+      enemy.onPlayerCollision = function () {            //creates function for when the player collide with the enemy
+        game.changeIntegrity(-20);    //when the player hits the enemy, they will lose 20 health
       };
 
-      enemy.onProjectileCollision = function (){
-        game.increaseScore(100);
-        enemy.shrink();
+      enemy.onProjectileCollision = function (){         //creates function for when the player shoots the nemy
+        game.increaseScore(100);      //when the player shoots the enemym they will gain 100 points
+        enemy.shrink();        //when the enemy is shot, it will shrink off the screen
       }
 
       
 
     }
 
-    function createReward(x, y){
-      var reward = game.createGameItem("enemy", 25);
-      var blueSquare = draw.rect (50, 50, "blue");
-      blueSquare.x = -25;
-      blueSquare.y = -25;
-      reward.addChild(blueSquare);
-      reward.x = x;
-      reward.y = y;
-      game.addGameItem(reward);
-      reward.velocityX = -4;
+    function createReward(x, y){                       //creates function for the reward
+      var reward = game.createGameItem("enemy", 25);         //creates the award in the game using the same as the enemy
+      var blueSquare = draw.rect (50, 50, "blue");   //draaws a blue square
+      blueSquare.x = -25;       // sets x value for the blue square
+      blueSquare.y = -25;           // sets y value for the blue square
+      reward.addChild(blueSquare);          //adds bluesquare to the game
+      reward.x = x;      //sets x value for the reward
+      reward.y = y;    //sets y value for the reward
+      game.addGameItem(reward);         //adds reward to the game
+      reward.velocityX = -4;          //speeds up the reward towards the left side of the screen
 
 
-      reward.onPlayerCollision = function (){
-        game.changeIntegrity(100);
-        game.IncreaseScore(100);
-        reward.shrink();
+      reward.onPlayerCollision = function (){         //creates the function for when the player collides with the reward
+        game.changeIntegrity(100);   //the player gains 100 health when it collides with the reward
+        game.IncreaseScore(100);    // the player gains 100 score when it collides with the reward
+        reward.shrink();     //the reward shrinks when it is collided with
       };
 
-      reward.onProjectileCollision = function (){
-       // game.increaseScore(10);
+      reward.onProjectileCollision = function (){         //creates function for when the reward gets shot
+       // game.increaseScore(10);    
        // reward.shrink();
       }
     }
 
-    function createMarker(x, y){
-      var marker = game.createGameItem("marker", 25);
-      var yellowSquare = draw.rect(50, 50, "yellow");
-      yellowSquare.x = -25;
-      yellowSquare.y = -25;
-      enemy.addChild(yellowSquare);
-      enemy.x = x;
-      enemy.y = y;
+    function createMarker(x, y){            //creates function for the marker
+      var marker = game.createGameItem("marker", 25);         //creates the marker in the game
+      var yellowSquare = draw.rect(50, 50, "yellow");      //draws a yellow square
+      yellowSquare.x = -25;          //sets x value for the yellow square
+      yellowSquare.y = -25;          //sets y value for the yellow square
+      enemy.addChild(yellowSquare);       //adds yellow sqquare to the game
+      enemy.x = x;        //sets x value for the marker
+      enemy.y = y;        //sets y value for the marker
       game.addGameItem(marker);
       enemy.velocityX = -4;
 
